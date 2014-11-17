56b17ae13bf922727d99a984f15f565bf4e0db39
beastMode <nicholas.desouza@uoit.net>
2014-11-17 16:35:37 -0500

    fixes: Player board get constantly updated when new users log in and join game. Removing the double game problem bugs: play again does not work.

--- backup/test.py
+++ backup/test.py
@@ -1,34 +1,6 @@
-
-
-board = [[True for x in range(5)] for y in range(5)]
-
-board[0][0] = False
-board[0][1] = False
-
-
-gameOver = True
-for e in range(0, 5):
-    for y in range(0, 5):
-
-        if y is 4:
-            if e is 4:
-                if not board[e][y] and not board[e+1][y]:
-                    gameOver = False
-                    break
-        elif e is 4:
-            if not board[e][y] and not board[e][y+1]:
-                gameOver = False
-                break
-        elif not board[e][y] and not board[e+1][y]:
-            gameOver = False
-            break
-        elif not board[e][y] and not board[e][y+1]:
-            gameOver = False
-            break
-        print e, y
-        if not gameOver:
-            break
-for x in range(5):
-    print board[x]
-
-print gameOver
+from py4j.java_gateway import JavaGateway                 
+gateway = JavaGateway()                                   
+entrypt = gateway.entry_point.getMove()             
+result = entrypt.myMove()
+for r in result:
+	print r
---
+++ backup/version5/test.py
@@ -0,0 +1,34 @@
+
+
+board = [[True for x in range(5)] for y in range(5)]
+
+board[0][0] = False
+board[0][1] = False
+
+
+gameOver = True
+for e in range(0, 5):
+    for y in range(0, 5):
+
+        if y is 4:
+            if e is 4:
+                if not board[e][y] and not board[e+1][y]:
+                    gameOver = False
+                    break
+        elif e is 4:
+            if not board[e][y] and not board[e][y+1]:
+                gameOver = False
+                break
+        elif not board[e][y] and not board[e+1][y]:
+            gameOver = False
+            break
+        elif not board[e][y] and not board[e][y+1]:
+            gameOver = False
+            break
+        print e, y
+        if not gameOver:
+            break
+for x in range(5):
+    print board[x]
+
+print gameOver
